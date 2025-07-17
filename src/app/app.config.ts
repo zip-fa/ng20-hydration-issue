6be2c8a42ac527b1225a42af6937d702c6d0c8c9
zip-fa <ikpocc@gmail.com>
2025-07-17 11:25:15 +0300

    removed routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,9 @@
 import {
   ApplicationConfig,
   provideAppInitializer,
-  provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection
 } from '@angular/core';
-import {provideRouter, withComponentInputBinding} from '@angular/router';
 
-import { routes } from './app.routes';
 import {provideClientHydration, withEventReplay, withI18nSupport} from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 
@@ -85,7 +82,6 @@ export const appConfig: ApplicationConfig = {
     }),
     provideZonelessChangeDetection(),
     provideClientHydration(withI18nSupport(), withEventReplay()),
-    provideHttpClient(withFetch()),
-    provideRouter(routes, withComponentInputBinding())
+    provideHttpClient(withFetch())
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,23 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  template: `
+    Hey<br/><br/><br/><br/><br/><br/>
+
+    <div style="display: flex; gap: 10px">
+      <!--<img ngSrc="non-existent-image.png" width="500" height="500" style="border: 1px solid black" />-->
+      <!--<img src="non-existent-image.png" width="500" height="500" style="border: 1px solid black" />-->
+
+      <img
+        [attr.data-test]="test()"
+        (click)="test.set(true)"
+        src="https://images.dog.ceo/breeds/papillon/n02086910_2994.jpg" width="500" height="500" style="border: 1px solid black" />
+    </div>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
+  test = signal(false);
 }
